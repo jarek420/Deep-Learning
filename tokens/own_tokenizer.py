0072e2564fcb097115759298c92c04ea6305d3d8
jarek420 <jaroslawgrzesiak21@gmail.com>
2025-08-04 13:45:32 +0200

    clean up code, rewrite some funciton to make them dynamic

--- tokens/own_tokenizer.py
+++ tokens/own_tokenizer.py
@@ -1,48 +1,49 @@
-import string
+def cut_string_to_tokens(s: str):
+    return list(s)
 
-test= "abbbababababbabababababbababbaaaabbbbaaaaaaaaaaaaabbabababbababbbabababa"
-
-# 1. def function that will cut string into array of chars "bakars" -> 'b', 'a', 'k', 'a', 'r', 's'
-def cut_string_to_chars(s: str):
-    char_array = []
-    for char in s:
-        char_array.append(char)
-    return char_array
-
-# 2. def function that will count all pairs of chars in array
-def count_pairs(char_array):
+def count_pairs(tokens):
     pairs_count = {}
-    for i in range(len(char_array) - 1):
-        pair = (char_array[i], char_array[i + 1])
+    for i in range(len(tokens) - 1):
+        pair = (tokens[i], tokens[i + 1])
         if pair in pairs_count:
             pairs_count[pair] += 1
         else:
             pairs_count[pair] = 1
     return pairs_count
 
-print(count_pairs(cut_string_to_chars(test)))
-
-# 3. def function that will create token from most common pair of chars
 def create_token_from_most_common_pair(pairs_count):
-    most_common_pair = max(pairs_count, key=pairs_count.get)
-    token = ''.join(most_common_pair)
-    return token
+    if not pairs_count:
+        return None
+    return max(pairs_count, key=pairs_count.get)
 
-def merge_tokens(token, char_array):
-    tmp_array = []
-    for i in range(len(char_array) - 1):
-        if char_array[i] == token[0] and char_array[i + 1] == token[1]:
-            tmp_array.append(token)
+def merge_tokens(most_common_pair, tokens):
+    merged = []
+    i = 0
+    while i < len(tokens):
+        if i < len(tokens) - 1 and (tokens[i], tokens[i+1]) == most_common_pair:
+            merged.append(tokens[i] + tokens[i+1])
+            i += 2
         else:
-            tmp_array.append(char_array[i])
-    return tmp_array
+            merged.append(tokens[i])
+            i += 1
+    return merged
 
-a = create_token_from_most_common_pair(count_pairs(cut_string_to_chars(test)))
-char_new = merge_tokens(a, cut_string_to_chars(test))
-print(merge_tokens(a, cut_string_to_chars(test)))
-#NOTE:
-# legth of the token will be changing, after chaging string into array of chars length of each elements is 1
-# so we need to make all functions more dynamic
-a = create_token_from_most_common_pair(count_pairs(cut_string_to_chars(char_new)))
-char_new = merge_tokens(a, cut_string_to_chars(char_new))
-print(merge_tokens(a, cut_string_to_chars(char_new)))
+def run_bpe(text, num_merges=10):
+    tokens = cut_string_to_tokens(text)
+    merges = []
+    for _ in range(num_merges):
+        pairs = count_pairs(tokens)
+        if not pairs:
+            break
+        most_common = create_token_from_most_common_pair(pairs)
+        merges.append(most_common)
+        tokens = merge_tokens(most_common, tokens)
+        print(f"Merged {most_common} -> tokens: {tokens}")
+    return tokens, merges
+
+# test
+text = "abbbababababbabababababbababbaaaabbbbaaaaaaaaaaaaabbabababbababbbabababa"
+tokens, merges = run_bpe(text, num_merges=15)
+
+print("\nFinal tokens:", tokens)
+print("Merge history:", merges)
